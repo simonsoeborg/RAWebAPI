1d9aeac747334f24cd8e7a9c138d04df89d0f4ae
Simon Søhår <s173255@student.dtu.dk>
2022-04-11 10:24:04 +0200

    Json naming on Model

--- Models/Auth.cs
+++ Models/Auth.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace RAWebAPI.Models
 {
@@ -12,15 +13,21 @@ namespace RAWebAPI.Models
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
         [StringLength(455)]
+        [JsonProperty(PropertyName = "Email")]
         public string Email { get; set; }
         public bool EmailVerified { get; set; }
         public string FamilyName { get; set; }
         public string GivenName { get; set; }
+        [JsonProperty(PropertyName = "Name")]
         public string Name { get; set; }
         public string Nickname { get; set; }
+        [JsonProperty(PropertyName = "Picture")]
         public string Picture { get; set; }
+        [JsonProperty(PropertyName = "Sub")]
         public string Sub { get; set; }
+        [JsonProperty(PropertyName = "Role")]
         public string Role { get; set; }
+        [JsonProperty(PropertyName = "Pin")]
         public int Pin { get; set; }
     }
 }
